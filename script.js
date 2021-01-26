d8d7aa9ec282dcd31442c78b18583ec7ba1ef202
swhachey <sw.hachey@gmail.com>
2021-01-25 20:33:54 -0700

    created a timer in the header that counts down from 75 when you click start

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var startButton = document.querySelector("#start-game-button");
+var questionText = document.querySelector("#container");
+var header = document.querySelector("#header");
+var questions = ["Why?", "Who?", "How?"];
+var secondsLeft = 75;
+var i=0;
+var r=1;
 
 function startQuiz() {
-    console.log("Fart")
-}
+ var questionAsked = questions.slice(0,1);
+ var timerInterval = setInterval(function() {
+    secondsLeft--;
+    header.textContent = secondsLeft;
+    if(secondsLeft === 0) {
+       
+        clearInterval(timerInterval);
+        
+      }}, 1000)
+questionText.textContent = questionAsked;
+};
 
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
