7987a65742fa4cea99319a4580c995001429098e
swhachey <sw.hachey@gmail.com>
2021-01-26 18:50:08 -0700

    got a bunch done on the functions aspect of my quiz game with my tutuor. added the questions and check answers functions

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 var startButton = document.querySelector("#start-game-button");
 var questionText = document.querySelector("#container");
+var answerContainer = document.querySelector("#answer-container")
 var header = document.querySelector("#header");
 var questions = [
-{prompt: "Why",
+{prompt: "Why?",
 choices: ["A", "B", "C", "D"],
 answer: "A",
 },
-{prompt: "How",
+{prompt: "How?",
 choices: ["A", "B", "C", "D"],
 answer: "B",
 },
-{prompt: "Who",
+{prompt: "Who?",
 choices: ["A", "B", "C", "D"],
 answer: "C",
 }
 ];
 var secondsLeft = 75;
 var i=0;
-var r=1;
+// var r=1;
 
 //this is my function that starts when you click the start button
 function startQuiz() {
- var questionAsked = questions.slice(0,1);
  //this is my time in the header
+//  questionText.setAttribute("class", "hide");
  var timerInterval = setInterval(function() {
     secondsLeft--;
     header.textContent = secondsLeft;
@@ -30,7 +31,77 @@ function startQuiz() {
         clearInterval(timerInterval);
       }}, 1000)
 //this is what shows in the container
-questionText.textContent = questionAsked;
+displayQuestion();
 };
 
+function displayQuestion(){
+  if (i >= 3) {
+    gameComplete();
+  } else {
+    var questionAsked = questions[i].prompt;
+  var choicesAvailable = questions[i].choices;
+    questionText.textContent = questionAsked;
+  }
+  
+  for (var j = 0; j < choicesAvailable.length; j++) {
+  var answersEl = document.createElement("button");
+  answersEl.addEventListener("click", answerChoice)
+  answersEl.textContent = choicesAvailable[j];
+  answerContainer.appendChild(answersEl);}
+ 
+};
+
+function answerChoice() {
+  answerContainer.textContent = "";
+  var chosenAnswer = this.textContent;
+  var correctAnswer = questions[i].answer;
+  if (chosenAnswer !== correctAnswer) {
+secondsLeft= secondsLeft - 15;
+i++;
+displayQuestion();
+  } else {
+    i++;
+displayQuestion();
+  }
+}
+
+function gameComplete (){
+  console.log("yup");
+  questionText.textContent = "";
+  // if (i >= questions.length) {
+  //   gameComplete();
+  // }
+    var score = document.createElement("h1")
+    score.textContent = "All Done!! Your Score was " + secondsLeft;
+    answerContainer.appendChild(score);
+}
+
 startButton.addEventListener("click", startQuiz);
+
+//JS
+//Data - Questions
+//Global Vars
+//Functions
+//Event Listeners
+
+//click start button
+//ref startButton on DOM
+//add onclick event listener
+//start timer
+//ref timer in DOM, use set interval
+//show timer in header
+
+//function displayQuestion
+//display question and choices
+//ref in dom
+//loop through questions
+//display questions on DOM
+//loop thru answers
+
+//function questionClick
+//check answer against choice
+//if answer correct, show next questions
+//if incorrect, subtract time
+// time - 15
+//when all questions answered, game over
+//when time reaches zero, game over
